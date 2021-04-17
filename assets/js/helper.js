5c6ffed267c472c8b4d55712f523955d910497dc
arubinofaux <arubinofaux@gmail.com>
2021-04-16 22:45:36 -0600

    update dark

--- assets/js/helper.js
+++ assets/js/helper.js
@@ -7,13 +7,17 @@ function formatCurrency(total) {
   return (neg ? "-$" : '$') + parseFloat(total, 10).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, "$1,").toString();
 }
 
-function b_diff(best) {
+function b_diff(best, dark=false) {
   const last_price = $(".price").attr("data-lastprice")
   const new_price = best
   let color = "#000000"
 
   if (new_price == last_price) {
     color = "#000000"
+
+    if (dark == true) {
+      color = "#ffffff"
+    }
   } else {
     if (new_price > last_price) {
       color = "#00c805"
@@ -22,6 +26,10 @@ function b_diff(best) {
     }
   }
 
+  render(new_price, color)
+}
+
+function render(new_price, color) {
   $(".price").attr("data-lastprice", new_price).css("color", color)
 
   $(".price").html(formatCurrency(new_price))
